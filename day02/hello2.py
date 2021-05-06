0958bcc80452e1c3f3b3d48c08726a566ac50d1c
pythonyiju <83127819+pythonyiju@users.noreply.github.com>
2021-05-06 20:00:47 +0800

    Initial commit

---
+++ day02/hello2.py
@@ -0,0 +1,9 @@
+#  -*-  coding:UTF-8  -*-
+chiness  =  "100"   # str
+english  = "90"    # str
+math  =  80       #  int
+print(chiness  ,english   ,math)
+print(type(chiness),  type(english),type(math))
+sum  = int(chiness) + int(english) + math
+print(“總分：”  +  str (sum) )
+
